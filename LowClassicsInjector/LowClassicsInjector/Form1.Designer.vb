81301d89989e866c47bd2e860421790124ff7854
Artur B <arturzubert@web.de>
2017-10-12 14:01:13 +0200

    Add files via upload

---
+++ LowClassicsInjector/LowClassicsInjector/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Process"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(98, 234)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Status"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(165, 234)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(11, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "*"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(129, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 80)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 109)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 138)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Clear All"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(129, 181)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Inject"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(235, 28)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "..."
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(101, 80)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(149, 95)
+        Me.ListBox1.TabIndex = 9
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(316, 28)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "Clear"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(257, 80)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(119, 17)
+        Me.CheckBox1.TabIndex = 11
+        Me.CheckBox1.Text = "Close after Injection"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 313)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Dll Injector by Artur"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+End Class
---
+++ LowClassicsInjector/LowClassicsInjector/Form1.vb
@@ -0,0 +1,140 @@
+﻿Public Class Form1
+
+    Dim dlls As New Dictionary(Of String, String)
+
+    Private Declare Function OpenProcess Lib "kernel32" (ByVal dwDesiredAccess As Integer, ByVal bInheritHandle As Integer, ByVal dwProcessId As Integer) As Integer
+    Private Declare Function VirtualAllocEx Lib "kernel32" (ByVal hProcess As Integer, ByVal lpAddress As Integer, ByVal dwSize As Integer, ByVal flAllocationType As Integer, ByVal flProtect As Integer) As Integer
+    Private Declare Function WriteProcessMemory Lib "kernel32" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByVal lpBuffer() As Byte, ByVal nSize As Integer, ByVal lpNumberOfBytesWritten As UInteger) As Boolean
+    Private Declare Function GetProcAddress Lib "kernel32" (ByVal hModule As Integer, ByVal lpProcName As String) As Integer
+    Private Declare Function GetModuleHandle Lib "kernel32" Alias "GetModuleHandleA" (ByVal lpModuleName As String) As Integer
+    Private Declare Function CreateRemoteThread Lib "kernel32" (ByVal hProcess As Integer, ByVal lpThreadAttributes As Integer, ByVal dwStackSize As Integer, ByVal lpStartAddress As Integer, ByVal lpParameter As Integer, ByVal dwCreationFlags As Integer, ByVal lpThreadId As Integer) As Integer
+    Private Declare Function WaitForSingleObject Lib "kernel32" (ByVal hHandle As Integer, ByVal dwMilliseconds As Integer) As Integer
+    Private Declare Function CloseHandle Lib "kernel32" (ByVal hObject As Integer) As Integer
+
+
+    Private Function Inject(ByVal pID As Integer, ByVal dlllocation As String) As Boolean
+
+
+        Dim hProcess As Integer = OpenProcess(&H1F0FFF, 1, pID)
+        If hProcess = 0 Then Return False
+        Dim dllBytes As Byte() = System.Text.Encoding.ASCII.GetBytes(dlllocation)
+        Dim allocAdress As Integer = VirtualAllocEx(hProcess, 0, dllBytes.Length, &H1000, &H4)
+        If allocAdress = Nothing Then Return False
+        Dim kernalMod As Integer = GetModuleHandle("kernel32.dll")
+        Dim loadLibAddr = GetProcAddress(kernalMod, "LoadLibraryA")
+        If kernalMod = 0 OrElse loadLibAddr = 0 Then Return False
+        WriteProcessMemory(hProcess, allocAdress, dllBytes, dllBytes.Length, 0)
+        Dim libThread As Integer = CreateRemoteThread(hProcess, 0, 0, loadLibAddr, allocAdress, 0, 0)
+
+        If libThread = 0 Then
+            Return False
+        Else
+            WaitForSingleObject(libThread, 5000)
+            CloseHandle(libThread)
+        End If
+        CloseHandle(hProcess)
+        Label3.Text = "Dll Has Successfully Injected!"
+        If CheckBox1.Checked = True Then
+            Me.Close()
+        End If
+
+        Return True
+    End Function
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Dim Filename As String = OpenFileDialog1.FileName.Substring(OpenFileDialog1.FileName.LastIndexOf("\"))
+        Dim DllFileName As String = Filename.Replace("\", "")
+        ListBox1.Items.Add(DllFileName)
+        dlls.Add(DllFileName, OpenFileDialog1.FileName)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ListBox1.Items.Count > 0 Then
+            Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox1.Text)
+            If TargetProcess.Length = 0 Then
+                Label3.Text = ("Watitng For.. " + TextBox1.Text + ".exe")
+
+            Else
+                Dim procID As Integer = Process.GetProcessesByName(TextBox1.Text)(0).Id
+                Timer1.Stop()
+                Timer2.Stop()
+
+                For Each inj As KeyValuePair(Of String, String) In dlls
+                    Inject(procID, inj.Value)
+                Next
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If TextBox1.Text = "" Then
+            Label3.Text = "Waiting For a Process"
+
+            Timer1.Stop()
+        ElseIf ListBox1.Items.Count = 0 Then
+            Label3.Text = "Waiting for a Dll Path"
+            Timer1.Stop()
+        Else
+            Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox1.Text)
+            If TargetProcess.Length = 0 Then
+                Label3.Text = ("Waiting for" + TextBox1.Text + ".exe")
+
+            Else
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Filter = "DLL (*.dll) |*.dll"
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox1.SelectedIndex >= 0 Then
+            OpenFileDialog1.Reset()
+            dlls.Remove(ListBox1.SelectedItem)
+            For i As Integer = (ListBox1.SelectedItems.Count - 1) To 0 Step -1
+                Dim i2 As Integer = i + 2
+                ListBox1.Items.Remove(ListBox1.SelectedItems(i))
+            Next
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
+        dlls.Clear()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If ListBox1.Items.Count > 0 Then
+            If TextBox1.Text <> "" Then
+                Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox1.Text)
+                If TargetProcess.Length = 0 Then
+                    MsgBox(TextBox1.Text + ".exe is not running", MsgBoxStyle.Critical, "Error!")
+                Else
+                    Timer1.Stop()
+                    Dim procID As Integer = Process.GetProcessesByName(TextBox1.Text)(0).Id
+
+
+                End If
+            Else
+                MsgBox(" You have not specified a process", MsgBoxStyle.Critical, "Error!")
+
+            End If
+        Else
+            MsgBox("You need to select a DLL file", MsgBoxStyle.Critical, " Error!")
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        OpenFileDialog2.Filter = "exe (*.exe) |*.exe|(*.*) |*.*"
+        OpenFileDialog2.ShowDialog()
+        TextBox1.Text = OpenFileDialog2.FileName
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Clear()
+    End Sub
+End Class
